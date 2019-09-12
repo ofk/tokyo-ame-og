853c30d246eadc0386be18d2a3419c918d55d3dc
ofk <ofkjpn+github@gmail.com>
2019-09-12 16:37:20 +0900

    Add percentage

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { URL } = require('url');
+const { URL, URLSearchParams } = require('url');
 const fetch = require('node-fetch');
 const { createCanvas, loadImage } = require('canvas');
 
@@ -25,28 +25,8 @@ ${imageContent}
 </html>`;
 };
 
-const indexPage = (url) => fetch(`https://tokyo-ame.jwa.or.jp/scripts/mesh_index.js?${Math.round(Date.now() / 150000)}`)
-      .then((res) => res.text())
-      .then((res) => {
-        const mt = /"(.*?)"/.exec(res);
-        const md = mt ? /(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})/.exec(mt[1]) : null;
-        return renderOgPage({
-          type: 'website',
-          image: mt ? `${url.origin}/image?t=${mt[1]}` : null,
-          url: 'https://tokyo-ame.jwa.or.jp/',
-          title: 'amesh now',
-          description: md ? `${md[1]}-${md[2]}-${md[3]} ${md[4]}:${md[5]}` : 'Umm.',
-        });
-      });
-
-const imagePage = (url) => Promise.all([
-  loadImage('https://tokyo-ame.jwa.or.jp/map/map150.jpg'),
-  loadImage(`https://tokyo-ame.jwa.or.jp/mesh/100/${url.searchParams.get('t')}.gif`),
-  loadImage('https://tokyo-ame.jwa.or.jp/map/msk150.png'),
-]).then((images) => {
-  const x = parseFloat(url.searchParams.get('x')) || 0.6;
-  const y = parseFloat(url.searchParams.get('y')) || 0.5;
-  const canvas = createCanvas(1200, 630);
+const mergeImages = (images, w, h, x, y) => {
+  const canvas = createCanvas(w, h);
   const ctx = canvas.getContext('2d');
   const widthRatio = canvas.width / images[0].width;
   const heightRatio = canvas.height / images[0].height;
@@ -60,7 +40,65 @@ const imagePage = (url) => Promise.all([
       0, 0, canvas.width, canvas.height
     );
   });
-  return canvas.toBuffer();
+  return canvas;
+};
+
+const indexPage = (url) => fetch(`https://tokyo-ame.jwa.or.jp/scripts/mesh_index.js?${Math.round(Date.now() / 150000)}`)
+      .then((res) => res.text())
+      .then((res) => {
+        const mt = /"(\d{12})"/.exec(res);
+        return mt ? mt[1] : Promise.reject();
+      })
+      .then((res) => Promise.all([
+        Promise.resolve(res),
+        loadImage(`https://tokyo-ame.jwa.or.jp/mesh/100/${res}.gif`),
+      ]))
+      .then(([res, img]) => {
+        const params = new URLSearchParams();
+        params.append('t', res);
+        ['x', 'y'].forEach((k) => {
+          if (url.searchParams.has(k)) {
+            params.append(k, url.searchParams.get(k));
+          }
+        });
+
+        const x = parseFloat(url.searchParams.get('x')) || 0.6;
+        const y = parseFloat(url.searchParams.get('y')) || 0.5;
+        const canvas = mergeImages([img], 600, 315, x, y);
+        const ctx = canvas.getContext('2d');
+        const imData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        let count = 0;
+        for (let i = 0; i < imData.data.length; i += 4) {
+          if (imData.data[i + 3]) {
+            count += 1;
+          }
+        }
+
+        const m = res ? /(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})/.exec(res) : null;
+        return {
+          image: `${url.origin}/image?${params}`,
+          description: m ? `${m[1]}-${m[2]}-${m[3]} ${m[4]}:${m[5]} (${((count * 100) / (canvas.width * canvas.height)).toFixed(2)}%)` : 'Umm.',
+        };
+      })
+      .catch(() => ({
+        image: null,
+        description: 'Umm.',
+      }))
+      .then((data) => renderOgPage({
+        ...data,
+        type: 'website',
+        url: 'https://tokyo-ame.jwa.or.jp/',
+        title: 'amesh now',
+      }));
+
+const imagePage = (url) => Promise.all([
+  loadImage('https://tokyo-ame.jwa.or.jp/map/map150.jpg'),
+  loadImage(`https://tokyo-ame.jwa.or.jp/mesh/100/${url.searchParams.get('t')}.gif`),
+  loadImage('https://tokyo-ame.jwa.or.jp/map/msk150.png'),
+]).then((images) => {
+  const x = parseFloat(url.searchParams.get('x')) || 0.6;
+  const y = parseFloat(url.searchParams.get('y')) || 0.5;
+  return mergeImages(images, 1200, 630, x, y).toBuffer();
 });
 
 module.exports = (req, res) => {
